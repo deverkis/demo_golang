78da98a909e7ef2258fdb338e39edd2b045a993b
deverkis <fjm20016@qq.com>
2022-09-28 21:56:22 +0800

    Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,21 @@ package main
 
 import (
     "fmt"
+    "log"
     "example.com/greetings"
 )
 
 func main() {
+    log.SetPrefix("greetings:")
+    log.SetFlags(0)
+
     //fmt.Println("Hello, World!")
-    message := greetings.Hello("Gladys")
+    //message := greetings.Hello("Gladys")
+    message, err := greetings.Hello("")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
     fmt.Println(message)
 }
