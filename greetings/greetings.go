0191a7ce4f40787a52a4181a161a7da9c81471c9
deverkis <fjm20016@qq.com>
2022-09-28 22:20:35 +0800

    Return greetings for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -15,6 +15,22 @@ func Hello(name string) (string, error) {
     return message, nil
 }
 
+func Hellos(names []string)(map[string]string, error){
+    // A map to associate names with messages.
+    messages := make(map[string]string)
+
+    // Loop through the received slice of names, calling
+    // the Hello function to get a message for each name.
+    for _,name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        messages[name] = message
+    }
+    return messages, nil
+}
+
 // Go executes init functions automatically at program startup
 func init(){
     rand.Seed(time.Now().UnixNano())
--- hello/hello.go
+++ hello/hello.go
@@ -13,11 +13,13 @@ func main() {
     //fmt.Println("Hello, World!")
     //message := greetings.Hello("Gladys")
     //message, err := greetings.Hello("")
-    message, err := greetings.Hello("Gladys")
-
+    //message, err := greetings.Hello("Gladys")
+    names := []string{"Gladys","Samantha","Darrin"}
+    messages, err := greetings.Hellos(names)
     if err != nil {
         log.Fatal(err)
     }
 
-    fmt.Println(message)
+    //fmt.Println(message)
+    fmt.Println(messages)
 }
